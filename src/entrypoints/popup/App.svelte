6e2f8018574fcddc3cda0f66c78a5a1196a6f48c
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-04-08 15:11:39 +0900

    chore: prepare initial submission

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import pkg from '../../../package.json';
+	import './app.css';
 </script>
 
-<main></main>
+<main>
+	{pkg.name} by <a href="https://github.com/hyunbinseo">{pkg.author}</a>
+</main>
 
 <style>
+	main {
+		padding: 2rem;
+	}
 </style>
