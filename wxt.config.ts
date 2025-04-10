e0ae09323d0fd3e34648d198d5a22f10d5c31189
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-04-10 16:53:57 +0900

    chore: use mv3 for firefox zip

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,14 @@
+import { env, loadEnvFile } from 'node:process';
 import { defineConfig } from 'wxt';
+
+loadEnvFile();
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	manifest: {
 		name: 'Better Surf',
 		description: '더 나은 웹 서핑을 위한 소소하지만 강력한 도구들',
+		browser_specific_settings: { gecko: { id: env.FIREFOX_EXTENSION_ID } },
 	},
 	srcDir: 'src',
 	modules: [
