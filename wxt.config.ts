ffde71f5557d7cf8ca96015df4de36c70877a1cd
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-07-30 10:08:37 +0900

    fix: specify firefox android compat

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,16 +5,22 @@ loadEnvFile();
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	manifest: {
+	manifest: ({ browser }) => ({
 		name: 'Better Surf',
 		description: '더 나은 웹 서핑을 위한 소소하지만 강력한 도구들',
 		browser_specific_settings: {
-			gecko: {
-				id: env.FIREFOX_EXTENSION_ID,
-				strict_min_version: '121.0', // CSS :has() support
-			},
+			...(browser === 'firefox' && {
+				gecko: {
+					id: env.FIREFOX_EXTENSION_ID,
+					strict_min_version: '121.0', // CSS :has() support
+				},
+				// To continue marking your extension as Android compatible on AMO,
+				// ensure that your manifest.json file includes a "browser_specific_settings.gecko_android" object.
+				// Reference https://blog.mozilla.org/addons/2023/10/05/changes-to-android-extension-signing
+				gecko_android: {},
+			}),
 		},
-	},
+	}),
 	vite: () => ({ build: { cssTarget: ['firefox121'] } }),
 	srcDir: 'src',
 	modules: [
@@ -25,10 +31,6 @@ export default defineConfig({
 	hooks: {
 		'build:manifestGenerated': (wxt, manifest) => {
 			manifest.content_scripts ??= [];
-			manifest.content_scripts.push({
-				css: ['content-scripts/moneypin-web.css'],
-				matches: ['https://web.moneypin.biz/*'],
-			});
 			manifest.content_scripts.push({
 				css: ['content-scripts/x.css'],
 				matches: ['https://x.com/*'],
