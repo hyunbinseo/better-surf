b1694c6057affc3715e37d04e5f6c5e88e4fa0f6
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-08-06 20:04:28 +0900

    feat: force pocket cu mobile web

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,28 +1,52 @@
 import { globSync, readFileSync } from 'node:fs';
 import { basename } from 'node:path';
 import { env, loadEnvFile } from 'node:process';
-import { defineConfig } from 'wxt';
+import { defineConfig, type UserManifest } from 'wxt';
 
 loadEnvFile();
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	manifest: ({ browser }) => ({
-		name: 'Better Surf',
-		description: '더 나은 웹 서핑을 위한 소소하지만 강력한 도구들',
-		browser_specific_settings: {
-			...(browser === 'firefox' && {
-				gecko: {
-					id: env.FIREFOX_EXTENSION_ID,
-					strict_min_version: '121.0',
-				},
-				// To continue marking your extension as Android compatible on AMO,
-				// ensure that your manifest.json file includes a "browser_specific_settings.gecko_android" object.
-				// Reference https://blog.mozilla.org/addons/2023/10/05/changes-to-android-extension-signing
-				gecko_android: {},
-			}),
-		},
-	}),
+	manifest: ({ browser }) => {
+		const manifest = {
+			name: 'Better Surf',
+			description: '더 나은 웹 서핑을 위한 소소하지만 강력한 도구들',
+			host_permissions: ['https://pocketcu.co.kr/*'],
+			permissions: ['declarativeNetRequest'],
+			declarative_net_request: {
+				rule_resources: [
+					{
+						id: 'mobile_user_agent',
+						path: 'mobile_user_agent.json',
+						enabled: true,
+					},
+				],
+			},
+			browser_specific_settings: {
+				...(browser === 'firefox' && {
+					gecko: {
+						id: env.FIREFOX_EXTENSION_ID,
+						strict_min_version: '121.0',
+					},
+					// To continue marking your extension as Android compatible on AMO,
+					// ensure that your manifest.json file includes a "browser_specific_settings.gecko_android" object.
+					// Reference https://blog.mozilla.org/addons/2023/10/05/changes-to-android-extension-signing
+					gecko_android: {},
+				}),
+			},
+		} satisfies UserManifest;
+
+		if (browser === 'firefox') {
+			manifest.host_permissions.push('https://*.swit.io/*');
+			manifest.declarative_net_request.rule_resources.push({
+				id: 'ff_user_agent',
+				path: 'ff_user_agent.json',
+				enabled: true,
+			});
+		}
+
+		return manifest;
+	},
 	vite: () => ({
 		build: {
 			cssTarget: ['firefox121'],
@@ -48,24 +72,6 @@ export default defineConfig({
 			}
 		},
 		'build:manifestGenerated': (wxt, manifest) => {
-			if (wxt.config.browser === 'firefox') {
-				//
-				manifest.host_permissions ??= [];
-				manifest.host_permissions.push('*://*.swit.io/*');
-				//
-				manifest.permissions ??= [];
-				manifest.permissions.push('declarativeNetRequest');
-				//
-				manifest.declarative_net_request ??= {
-					rule_resources: [
-						{
-							id: 'ff_user_agent',
-							path: 'ff_user_agent.json',
-							enabled: true,
-						},
-					],
-				};
-			}
 			manifest.content_scripts ??= [];
 			manifest.content_scripts.push({
 				css: ['content-scripts/x.css'],
