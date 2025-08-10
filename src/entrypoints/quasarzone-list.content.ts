4b8dfc4f357142dbc1ba91170a3d06e90a9e9474
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-08-10 10:44:36 +0900

    feat: force quasarzone deal thumbnails

---
+++ src/entrypoints/quasarzone-list.content.ts
@@ -0,0 +1,17 @@
+export default defineContentScript({
+	matches: [
+		'https://quasarzone.com/bbs/qb_saleinfo', //
+		'https://quasarzone.com/bbs/qb_saleinfo?*',
+	],
+	main: () => {
+		const isMobile = !!document.querySelector('header#top-header');
+		if (!isMobile) return;
+
+		const url = new URL(window.location.href);
+		const viewType = url.searchParams.get('view_type');
+		if (viewType === 'list2') return;
+
+		url.searchParams.set('view_type', 'list2');
+		window.location.href = url.toString();
+	},
+});
