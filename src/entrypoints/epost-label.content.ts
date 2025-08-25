2df4a603899ad3cc6c535bb46501e64217d0c005
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-08-25 17:20:36 +0900

    chore: add epost inquiry response

--- src/entrypoints/epost-label.content.ts
+++ src/entrypoints/epost-label.content.ts
@@ -1,3 +1,7 @@
+// 우정사업본부 우정정보관리원 인터넷우체국팀 (2025-08-25 16:31:23)
+// 현재 인터넷우체국에서는 간편사전접수(등기통상) 주소라벨 인쇄서비스를 제공하고 있으며, 라벨 인쇄 과정에서 환경설정 등 유의사항을 팝업으로만 안내해 드리고 있습니다.
+// 이에 이용자가 별도 팝업창 없이도 인쇄 유의사항을 확인할 수 있도록 주소라벨 인쇄 본문페이지 내에 해당 안내사항을 포함하여 개선 조치(‘25년 10월 중)하도록 하겠습니다.
+
 export default defineContentScript({
 	matches: ['https://service.epost.go.kr/front.commonpostplus.AcceptPlusLabelPrint.postal'],
 	main: () => {
