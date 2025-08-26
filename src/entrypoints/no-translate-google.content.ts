4c2a81a4a7efe4d0023e8c668afeef6e0d49d5cb
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-08-26 22:52:54 +0900

    fix: empty string in url constructor

--- src/entrypoints/no-translate-google.content.ts
+++ src/entrypoints/no-translate-google.content.ts
@@ -10,12 +10,14 @@ export default defineContentScript({
 	runAt: 'document_start',
 	main: async () => {
 		const url = new URL(window.location.href);
-		const referrer = new URL(document.referrer);
 
 		// referrer is an empty string in Firefox.
 		const switchToEnglish = import.meta.env.FIREFOX
-			? url.searchParams.has('hl') && window.confirm('Switch to English?')
-			: url.hostname !== referrer.hostname && url.searchParams.has('hl');
+			? url.searchParams.has('hl') && //
+				window.confirm('Switch to English?')
+			: document.referrer &&
+				url.hostname !== new URL(document.referrer).hostname &&
+				url.searchParams.has('hl');
 
 		if (!switchToEnglish) return;
 		await cookieStore.set('django_language', 'en');
