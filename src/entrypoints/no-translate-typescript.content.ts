d4014d2ac53df260436d1f2b352ec0fec3b93188
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-11-30 15:49:30 +0900

    feat: default to en on typescript docs

---
+++ src/entrypoints/no-translate-typescript.content.ts
@@ -0,0 +1,18 @@
+export default defineContentScript({
+	matches: ['https://www.typescriptlang.org/ko/*'],
+	runAt: 'document_start',
+	main: () => {
+		const url = new URL(window.location.href);
+
+		// referrer is an empty string in Firefox.
+		const switchToEnglish = import.meta.env.FIREFOX
+			? window.confirm('Switch to English?')
+			: document.referrer && //
+				url.hostname !== new URL(document.referrer).hostname;
+
+		if (!switchToEnglish) return;
+
+		url.pathname = url.pathname.replace(/^\/ko\//, '/');
+		window.location.href = url.toString();
+	},
+});
