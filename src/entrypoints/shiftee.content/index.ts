1bae3faefa42a5d05aa910432ba1d606cb2f8f7f
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-02-09 11:57:39 +0900

    fix: global shiftee css for client navigation

--- src/entrypoints/shiftee.content/index.ts
+++ src/entrypoints/shiftee.content/index.ts
@@ -1,6 +1,8 @@
 import './style.css';
 
 export default defineContentScript({
-	matches: ['https://shiftee.io/app/companies/*'],
+	// NOTE Landing page [로그인] anchor is handled by client-side navigation
+	// Custom styles should be applied when redirected to /app in this manner
+	matches: ['https://shiftee.io/*'],
 	main: () => {},
 });
