5f671974bf09986a140272e4a168fd523a1ac9c8
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-04-20 11:14:32 +0900

    feat(firefox-redirect): add korean digital receipt

--- src/entrypoints/firefox-redirect.content.ts
+++ src/entrypoints/firefox-redirect.content.ts
@@ -11,11 +11,13 @@ export default defineContentScript({
 	matches: [
 		'https://gift-talk.kakao.com/appredirect?to=*',
 		'https://qr.kakaopay.com/*',
+		'https://receipt.allink.io/*', // 소설 로그인 후 휴대폰 인증 불가, 빈 화면 표시됨
 		'https://shoppinglive.kakao.com/bridge?redirect=*',
 	],
 	runAt: 'document_start',
 	main: (ctx) => {
 		redirect(new URL(window.location.href));
+		// e.g. 카카오 쇼핑라이브를 둘러보다가 '카카오톡에서 보기' [앱 열기] 버튼을 누르는 경우
 		ctx.addEventListener(window, 'wxt:locationchange', ({ newUrl }) => redirect(newUrl));
 	},
 });
