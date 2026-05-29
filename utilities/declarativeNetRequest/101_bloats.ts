b6c6ff9754752517e55c37c3dbb6c1c901510c46
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-05-29 19:49:07 +0900

    feat(bloats): strip tracking query params

--- utilities/declarativeNetRequest/101_bloats.ts
+++ utilities/declarativeNetRequest/101_bloats.ts
@@ -9,6 +9,88 @@ export const rule_bloats: chrome.declarativeNetRequest.Rule[] = [
 			redirect: {
 				transform: {
 					queryTransform: {
+						removeParams: [
+							'ad_id',
+							'campaign_id',
+							'utm_campaign',
+							'utm_content',
+							'utm_id',
+							'utm_medium',
+							'utm_source',
+							'utm_term',
+
+							'_openstat', // Yandex
+							'dclid', // Google
+							'epik', // Pinterest
+							'fbclid', // Facebook
+							'gbraid', // Google
+							'gclid', // Google
+							'igshid', // Instagram
+							'li_fat_id', // LinkedIn
+							'mc_eid', // Mailchimp
+							'mkt_tok', // Marketo
+							'msclkid', // Microsoft
+							'rb_clickid', // Rakuten
+							'rdt_cid', // Reddit
+							's_cid', // Adobe
+							'ScCid', // Snapchat
+							'srsltid', // Google
+							'ttclid', // TikTok
+							'twclid', // Twitter/X
+							'wbraid', // Google
+							'yclid', // Yandex
+						],
+					},
+				},
+			},
+		},
+		condition: {
+			resourceTypes: ['main_frame'],
+		},
+	},
+	{
+		id: 0,
+		action: {
+			type: 'redirect',
+			redirect: {
+				transform: {
+					queryTransform: {
+						removeParams: [
+							'airbridge_referrer',
+							'ad_creative_id',
+							'ad_group',
+							'ad_group_id',
+							'adfrom',
+							'campaign',
+							'campaign_id',
+							'click_id',
+							'gaid_raw',
+							'ifa_raw',
+							'routing_short_id',
+							'sub1',
+							'sub2',
+							'sub_id',
+							'tracking_template_id',
+							'utm_campaign',
+							'utm_medium',
+							'utm_source',
+						],
+					},
+				},
+			},
+		},
+		condition: {
+			regexFilter: '[?&]airbridge_referrer=',
+			resourceTypes: ['main_frame'],
+		},
+	},
+	{
+		id: 0,
+		action: {
+			type: 'redirect',
+			redirect: {
+				transform: {
+					queryTransform: {
 						removeParams: ['redir_token'],
 					},
 				},
