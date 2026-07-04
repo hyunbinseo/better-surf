c1b34042c0508361e2f355ce84d52df77a8d9342
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-07-04 09:41:30 +0900

    refactor: check user-agent in content script init

--- src/entrypoints/firefox-redirect.content.ts
+++ src/entrypoints/firefox-redirect.content.ts
@@ -1,10 +1,12 @@
 const redirect = (url: URL) => {
-	if (!navigator.userAgent.startsWith('Mozilla/5.0 (Android')) return;
 	url.hash = `#Intent;scheme=${url.protocol.slice(0, -1)};package=com.android.chrome;end`;
 	// Cannot be changed using the URL protocol property
 	// See https://github.com/whatwg/url/issues/674
 	window.location.href = 'intent:' + url.href.slice(url.protocol.length);
 };
+
+// NOTE Check if ad block is interfering with open in app
+// See https://github.com/hyunbinseo/better-surf/issues/15
 
 export default defineContentScript({
 	include: ['firefox'],
@@ -18,6 +20,7 @@ export default defineContentScript({
 	],
 	runAt: 'document_start',
 	main: (ctx) => {
+		if (!navigator.userAgent.startsWith('Mozilla/5.0 (Android')) return;
 		redirect(new URL(window.location.href));
 		// e.g. 카카오 쇼핑라이브를 둘러보다가 '카카오톡에서 보기' [앱 열기] 버튼을 누르는 경우
 		ctx.addEventListener(window, 'wxt:locationchange', ({ newUrl }) => redirect(newUrl));
