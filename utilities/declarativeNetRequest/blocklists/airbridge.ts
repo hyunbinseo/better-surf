e73b76ba734f7565675b471edc5d9072283f2dba
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-08-22 21:45:47 +0900

    fix(airbridge): allow deeplink redirect

--- utilities/declarativeNetRequest/blocklists/airbridge.ts
+++ utilities/declarativeNetRequest/blocklists/airbridge.ts
@@ -1,6 +1,18 @@
 export const rules: chrome.declarativeNetRequest.Rule[] = [
 	{
 		id: 0,
+		action: { type: 'allow' },
+		condition: {
+			// e.g. https://static.airbridge.io/sdk/latest/airbridge.deeplink_page.min.js
+			// e.g. https://static.airbridge.io/sdk/latest/airbridge.deeplink_compatibility.min.js
+			// e.g. https://static.airbridge.io/sdk/latest/airbridge.deeplink.min.js
+			requestDomains: ['airbridge.io'],
+			urlFilter: '/airbridge.deeplink',
+			resourceTypes: ['script'],
+		},
+	},
+	{
+		id: 0,
 		action: { type: 'block' },
 		condition: {
 			// e.g. https://static.airbridge.io/sdk/latest/airbridge.min.js
